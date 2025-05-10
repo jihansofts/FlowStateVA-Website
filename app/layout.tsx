924fe95db11f09fe127a999a43ed002e6b0c4abb
jihansoft <jihanuddinsoft@gmail.com>
2025-05-11 00:16:38 +0600

    fix dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider attribute="class" enableSystem disableTransitionOnChange>
+        <ThemeProvider enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
-import Image from "next/image";
-import Link from "next/link";
-
-import { ArrowRight } from "lucide-react";
 import Navbar from "../components/Navbar";
 import Hero from "@/components/Hero";
 import WhyChoose from "@/components/WhyChoose";
